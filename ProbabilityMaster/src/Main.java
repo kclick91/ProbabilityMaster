1df4734754ab64fea6d0fbdd13290a91cc05d5e4
Kelly Click <kclick91@gmail.com>
2021-11-17 12:49:15 -0500

    Add files via upload

--- ProbabilityMaster/src/Main.java
+++ ProbabilityMaster/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("WELCOME TO A WEIGHTED RANDOM NUMBER GENERATOR: ");
+        System.out.println("WELCOME TO A NON-UNIFORM RANDOM NUMBER GENERATOR: ");
         int[] pr = new int[]{1,1,1};
         int[] prTwo = new int[]{5,1,1};
         int[] prThree = new int[]{1,2,9,3,4};
@@ -13,12 +13,27 @@ public class Main {
         {
             prFour[j] = j + 1;
         }
+        int[] prFive = new int[1000];
+        for (int j = 0; j < 1000; j++)
+        {
+            if (j < 500)
+            {
+                prFive[j] = 500 - (500 - (j + 1));
+            }
+            else if (j >= 500)
+            {
+                prFive[j] = 500 - ((j + 1) - 500);
+            }
+
+        }
         //Parameters are minimum, maximum, and weights
         TestProbability tp = new TestProbability(1,3,pr);
         TestProbability tpTwo = new TestProbability(1,3,prTwo);
         TestProbability tpThree = new TestProbability(1,5,prThree);
         //Numbers closer to 1000 have greater weight
         TestProbability tpFour = new TestProbability(1, 1000, prFour);
+        //Numbers closest to 500 have the greatest weight
+        TestProbability tpFive = new TestProbability(1, 1000, prFive);
         tp.PrintValues(3);
         tp.PrintWeight();
         System.out.println();
@@ -33,6 +48,8 @@ public class Main {
         System.out.println();
         tpFour.PrintValues(20);
         tpFour.PrintValues(20);
+        tpFive.PrintWeight();
+        tpFive.PrintValues(10);
 
 
 
