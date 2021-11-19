9e2a2764b93392a1a0cad33acd4ee78b347efcdf
Kelly Click <kclick91@gmail.com>
2021-11-19 10:47:07 -0500

    Add files via upload

--- ProbabilityMaster/src/Main.java
+++ ProbabilityMaster/src/Main.java
@@ -85,17 +85,27 @@ public class Main {
         tpML.PrintValues(3);
         tpML.PrintValues(3);
         System.out.println("FINISHED with 2 examples listed.");
+
+
         //Atmosphere Composition of Earth//
         int[] atmosphereWeights = new int[]{7808, 2095, 93, 4};
 
+        System.out.println("Samples from Earth's atmosphere by mole fraction: ");
         HashMap<Integer, String> mapAtmosphere = new HashMap<Integer, String>();
         mapAtmosphere.put(1, "Nitrogen");
         mapAtmosphere.put(2, "Oxygen");
         mapAtmosphere.put(3, "Argon");
         mapAtmosphere.put(4, "Carbon Dioxide");
         TestProbability tpAtmosphere = new TestProbability(1, 4, atmosphereWeights,mapAtmosphere);
+        System.out.println("Sample 1: ");
+        tpAtmosphere.PrintValues(20);
+        System.out.println("Sample 2: ");
+        tpAtmosphere.PrintValues(20);
+        System.out.println("Sample 3: ");
         tpAtmosphere.PrintValues(20);
         tpAtmosphere.PrintWeight();
+
+
 
 
 
